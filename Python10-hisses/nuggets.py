340b56e431b4880c231d0cafb65eb79220a0c92f
Artymanprince <adebiyiitunuayo@gmail.com>
2022-03-06 21:15:04 +0100

    Format

--- Python10-hisses/nuggets.py
+++ Python10-hisses/nuggets.py
@@ -1,30 +1,25 @@
-<h2 text-align="center">Hisses</h2>
-<p>
-Developed in <b>1990</b>, Python is one of the most popular <i>general-purpose</i> programming languages in modern times.
+"""
+Hisses
+
+Developed in 1990, Python is one of the most popular general-purpose programming languages in modern times.
 
 
 The term “general-purpose” simply means that Python can be used for a variety of applications and does not focus on any one aspect of programming. 
-</p> <br>
+"""
 
-To display data/information
-
-print (data)
-
-
-Use case:
-	To display <strong>Strings</strong>(group of characters enclosed in single or double quotes) <br/>
+	To display Strings (group of characters enclosed in single or double quotes) 
 print('Hello World') // Hello World
 print("Hello World") // Hello World
 
-	To display <strong>Numbers</>
+	To display Numbers
 print(50) // 50
 print(1000) // 1000
 print(3.142) // 3.142
 
 	To display multiple data
-print(50, 1000, 3.142, "Hello World", 'hello') // 50, 1000, 3.142, Hello World, hello 
+print(50, 1000, 3.142, "Hello World", 'hello') # 50, 1000, 3.142, Hello World, hello 
 
-By default Python prints <code>print</code> statements on new lines hence to join these  <code>print</code> statements use the  <code>end</code> attribute with whatever value you wish.
+By default Python prints 'print'  statements on new lines hence to join these 'print'  statements use the 'end'  attribute with whatever value you wish.
 Use case:
 print("Hello, come",  end=" ")
 print("World") // Hello, come World
@@ -35,7 +30,7 @@ print("World") // Hello World
 print("Hello", end="") 
 print("World") // HelloWorld
 
-	To Comment using the  hash <code>#</code> for single-line and triple quotations <code>""" """</code> for multi-line
+	To Comment using the  hash (#)  for single-line and triple quotations """ """ for multi-line
 Use case:
 print(50)  # This line prints 50
 print("Hello World")  # This line prints Hello World
@@ -63,11 +58,11 @@ Booleans (true, false)
 	
 Naming convention:
 
-<em>Numbers</em> must not begin a variable name but can appear anywhere else: For example, 12income✖️ is not a valid name but income12✔️ or in12come✔️ are valid.
+Numbers must not begin a variable name but can appear anywhere else: For example, 12income✖️ is not a valid name but income12✔️ or in12come✔️ are valid.
 
-<em>Case sensitivity</em>: For example, Income and income are two different variables and not one.
+Case sensitivity: For example, Income and income are two different variables and not one.
 You can define your income variable as Income or income, both are valid.
 
-<em>Underscores can appear anywhere</em>: For example, _income or income_ are valid name, monthly_income is a valid name.
+Underscores can appear anywhere: For example, _income or income_ are valid name, monthly_income is a valid name.
 
-<em>Variables must give meaningful information</em>: For example, inc or even income would not give any useful information but names like weekly_income, monthly_income, or annual_income explain the purpose of our defined variable. 
+Variables must give meaningful information: For example, inc or even income would not give any useful information but names like weekly_income, monthly_income, or annual_income explain the purpose of our defined variable. 
